283dedaaeee918dac14775e8c01715b41220182d
Caetlyn Venche <caetlynvenche@gmail.com>
2020-03-18 13:34:39 -0600

    worked on carousel, styling, login page, pulling in more information, font choice.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import './style/style.css'
-import { FaInstagram } from 'react-icons/fa'
+import { FaInstagram, FaMixcloud } from 'react-icons/fa'
 import Header from './components/Header';
 import Home from './components/Home'
-import Contact from './components/Contact'
 import EventPage from './components/EventPage'
 import Login from './components/Login'
 import SideLink from './components/SideLink'
@@ -17,15 +16,13 @@ function App() {
 
 		<Switch>
         	<Route exact path="/"><Home /></Route>
-			<Route path="/contact"><Contact /></Route>
 			<Route path="/event"><EventPage /></Route>
 			<Route path="/login"><Login /></Route>
     	</Switch>
 
 		<div className="sideLinkSection">
-			<SideLink linkHref="a" sideLinkSymbol={<FaInstagram />} linkName="Instagram" />
-			<SideLink linkHref="a" sideLinkSymbol={<FaInstagram />} linkName="Instagram" />
-			<SideLink linkHref="a" sideLinkSymbol={<FaInstagram />} linkName="Instagram" />
+			<SideLink linkHref="https://www.instagram.com/djbentleytaylor/" sideLinkSymbol={<FaInstagram />} linkName="Instagram" />
+			<SideLink linkHref="https://www.mixcloud.com/djbentleytaylor/?fbclid=IwAR1aNp9CH-UU4BlzRir3YRx2kkqowpty5x0VjetsFKsnl2x3JJHRjfziqHM" sideLinkSymbol={<FaMixcloud />} linkName="Mixcloud" />
 		</div>
     	<Footer />
     	</div>
--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image } from 'pure-react-carousel'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image, DotGroup } from 'pure-react-carousel'
 import 'pure-react-carousel/dist/react-carousel.es.css';
+import { FaAngleRight, FaAngleLeft } from 'react-icons/fa'
 
 export default class Carousel extends React.Component {
     render() {
@@ -8,23 +9,33 @@ export default class Carousel extends React.Component {
         <CarouselProvider
             naturalSlideWidth={100}
             naturalSlideHeight={35}
-            totalSlides={3}
-            className="carousel"
-            isPlaying="true"
+            totalSlides={4}
+            className="myCarousel"
+            // isPlaying="true"
             infinite
             visibleSlides="2"
         >
+            
             <Slider>
                 {/* have pictures vary in how they are arrayed? have a modal effect? */}
                 <Slide index={0}>I am the first Slide.</Slide>
                 <Slide index={1}>I am the second Slide.</Slide>
                 <Slide index={2}>I am the third Slide.</Slide>
+                <Slide index={3}>I am the fourth Slide.</Slide>
             </Slider>
 
             {/* add dot component? */}
 
-            <ButtonBack>Back</ButtonBack>
-            <ButtonNext>Next</ButtonNext>
+            <div className="carouselNav">
+                <ButtonBack className="carouselBtn"><FaAngleLeft /></ButtonBack>
+                <DotGroup 
+                    className="carouselDots" 
+                    showAsSelectedForCurrentSlideOnly="true"
+                    />
+                <ButtonNext className="carouselBtn"><FaAngleRight /></ButtonNext>
+            </div>
+
+            
         </CarouselProvider>
         );
     }
