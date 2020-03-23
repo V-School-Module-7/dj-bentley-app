d2df882c68c803d265bd9c400e875020382f756d
Laura Daybell <laura.l.daybell@gmail.com>
2020-03-23 10:43:49 -0600

    test connection

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Button = (props) => {
     return (
         <button className="btn" onClick={props.btnAction}>{props.btnContent}</button>
+        // test
     )
 }
 
