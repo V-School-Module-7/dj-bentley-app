81027e395b7323697d9faa8a0ae44ef8fb6e2d2e
Caetlyn Venche <caetlynvenche@gmail.com>
2020-05-06 15:45:48 -0600

    added banner ends to fellow dj subtitle. Formatted and placed names and created functionality to have it cycle through

--- client/src/components/MusicPlayer.js
+++ client/src/components/MusicPlayer.js
@@ -17,7 +17,15 @@ const MusicPlayer = () => {
                 <iframe title="mixcloud player" width="100%" height="400" src={song} frameBorder="0" ></iframe>
             </div>
 
-            <div>
+            <div className='musicList'>
+                <div className="otherMusic" onClick={() => handleSongChange("https://www.mixcloud.com/widget/iframe/?feed=%2Fdjbentleytaylor%2Fdj-bentley-top-40-dance-mix%2F")}>
+                    <img alt="DJ Bentley Top 40 Dance Mix" src="https://thumbnailer.mixcloud.com/unsafe/600x600/extaudio/d/6/3/1/3fd1-8887-446a-a0b7-ee924484feeb.jpg" />
+                    <h3>DJ Bentley Top 40 Dance Mix</h3>
+                </div>
+                <div className="otherMusic" onClick={() => handleSongChange("https://www.mixcloud.com/widget/iframe/?feed=%2Fdjbentleytaylor%2Fdj-bentley-top-40-dance-mix%2F")}>
+                    <img alt="DJ Bentley Top 40 Dance Mix" src="https://thumbnailer.mixcloud.com/unsafe/600x600/extaudio/d/6/3/1/3fd1-8887-446a-a0b7-ee924484feeb.jpg" />
+                    <h3>DJ Bentley Top 40 Dance Mix</h3>
+                </div>
                 <div className="otherMusic" onClick={() => handleSongChange("https://www.mixcloud.com/widget/iframe/?feed=%2Fdjbentleytaylor%2Fdj-bentley-top-40-dance-mix%2F")}>
                     <img alt="DJ Bentley Top 40 Dance Mix" src="https://thumbnailer.mixcloud.com/unsafe/600x600/extaudio/d/6/3/1/3fd1-8887-446a-a0b7-ee924484feeb.jpg" />
                     <h3>DJ Bentley Top 40 Dance Mix</h3>
