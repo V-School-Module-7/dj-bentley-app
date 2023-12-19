e2d69aa9f0b9ff751f65bbe7907663de5524f536
Ashley McKellar <ashley.l.mckellar@gmail.com>
2023-12-19 17:23:15 -0600

    added about me and mixplayer

---
+++ client/componenets/About.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const About = () => {
+    const [bio, setBio] = useState("");
+
+    useEffect(() => {
+        axios
+            .get("https://api.mixcloud.com/djbentleytaylor")
+            .then((res) => {
+                const data = res.data.biog;
+                console.log(data);
+                setBio(data);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
+    return (
+        <div className="aboutPage">
+            <div className="aboutImage"></div>
+            <div className="aboutContent">
+                <h1>ABOUT ME</h1>
+                <p>{bio}</p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
---
+++ client/componenets/MixPlayer.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+// import IndMusic from './IndMusic'
+
+const MusicPlayer = () => {
+    // const { dataState } = useContext(UserContext);
+    const [mixes, setMixes] = useState([]);
+    const [selectedMix, setSelectedMix] = useState(null);
+    const [isClicked, setIsClicked] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    "https://api.mixcloud.com/djbentleytaylor/cloudcasts"
+                );
+                const data = response.data.data;
+                console.log(data);
+                const mixTiles = data.map((mix) => ({
+                    name: mix.name,
+                    url: mix.url,
+                    pictures: mix.pictures && mix.pictures["320wx320h"],
+                }));
+
+                setMixes(mixTiles);
+            } catch (error) {
+                console.error("Error fetching Mixcloud data:", error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    const handleMixClick = (index) => {
+        setSelectedMix(index);
+        setIsClicked(true);
+    };
+
+    const handleBlur = () => {
+        setIsClicked(false);
+        setSelectedMix(null);
+    };
+
+    return (
+        <div className="musicPlayer">
+            <div>
+                {mixes.map((mix, index) => (
+                    <div
+                        className="mix-box"
+                        key={index}
+                        onClick={() => handleMixClick(index)}
+                    >
+                        {selectedMix !== null && isClicked ? (
+                            <iframe
+                                width="100%"
+                                height="400"
+                                title={mixes[selectedMix].name}
+                                src={`https://player-widget.mixcloud.com/widget/iframe/?light=1&feed=${encodeURIComponent(
+                                    mix.url
+                                )}`}
+                                frameBorder="0"
+                                onBlur={handleBlur}
+                            ></iframe>
+                        ) : (
+                            <div>
+                                <h3>{mix.name}</h3>
+                                <img src={mix.pictures} alt={mix.name} />
+                            </div>
+                        )}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default MusicPlayer;
