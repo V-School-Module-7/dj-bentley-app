5086ba2de939e9b6b62a19b70a2bd108cd6388de
Ashley McKellar <ashley.l.mckellar@gmail.com>
2024-01-03 19:26:54 -0800

    tried unsuccessfully to debug mixplayer focus issue

--- client/componenets/MixPlayer.jsx
+++ client/componenets/MixPlayer.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
+import ReactPlayer from 'react-player'
 // import IndMusic from './IndMusic'
 
 const MixPlayer = () => {
@@ -7,6 +8,8 @@ const MixPlayer = () => {
     const [mixes, setMixes] = useState([]);
     const [selectedMix, setSelectedMix] = useState(null);
     const [isClicked, setIsClicked] = useState(false);
+
+    const iframeRef = useRef(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -22,7 +25,8 @@ const MixPlayer = () => {
                     pictures: mix.pictures && mix.pictures["320wx320h"],
                 }));
 
-                setMixes(mixTiles);
+                setMixes(mixTiles)
+                console.log(mixes);
             } catch (error) {
                 console.error("Error fetching Mixcloud data:", error);
             }
@@ -40,8 +44,27 @@ const MixPlayer = () => {
         setSelectedMix(null);
     };
 
+    const handleMouseOver = () => {
+        if (iframeRef.current) {
+            console.log('moused over')
+            iframeRef.current.contentWindow.postMessage({ method: 'play' }, '*');
+        }
+    };
+
+    const handleMouseOut = () => {
+        if (iframeRef.current) {
+            console.log("moused out")
+            // Pause the Mixcloud player when the iframe is not hovered
+            iframeRef.current.contentWindow.postMessage({ method: 'pause' }, '*');
+        }
+    };
+        
+    
+
     return (
+        
         <div className="musicPlayer">
+            <script src="//widget.mixcloud.com/media/js/widgetApi.js" type="text/javascript"></script>
             <div>
                 {mixes.map((mix, index) => (
                     <div
@@ -51,7 +74,7 @@ const MixPlayer = () => {
                     >
                         {selectedMix !== null && isClicked ? (
                             <iframe
-                                width="100%"
+                                width="30%"
                                 height="400"
                                 title={mixes[selectedMix].name}
                                 src={`https://player-widget.mixcloud.com/widget/iframe/?light=1&feed=${encodeURIComponent(
@@ -59,6 +82,8 @@ const MixPlayer = () => {
                                 )}`}
                                 frameBorder="0"
                                 onBlur={handleBlur}
+                                onMouseOver={handleMouseOver}
+                                onMouseOut={handleMouseOut}
                             ></iframe>
                         ) : (
                             <div>
