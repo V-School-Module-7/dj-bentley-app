25ce26c12c8bfc87f057465cc53ba32338d5049a
valerie smith <valerie.plante.11@gmail.com>
2024-01-04 14:21:31 -0500

    react scroll is functional

--- client/componenets/App.jsx
+++ client/componenets/App.jsx
@@ -1,9 +1,11 @@
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Link, animateScroll as scroll } from "react-scroll";
 import MixPlayer from './MixPlayer.jsx'
 import About from './About.jsx'
 import Reviews from './Reviews.jsx';
 import Gallery from './Gallery.jsx';
 import GalleryImage from './GalleryImage.jsx';
+import Home from './Home.jsx'
 import'./style.css'
 import BookingForm from './BookingForm.jsx';
 
@@ -16,22 +18,83 @@ function App() {
           <nav className='nav-links'>
           <img src ="images/NavBarLogo.png" className='logo'/>
           <div className = "navItems">
-          <Link to="/" className="nav-link">Home</Link>
-          <Link to="/about" className="nav-link">Bio</Link>
-          <Link to="/mixplayer" className="nav-link">DJ Mixes</Link>
-          <Link to="/booking" className="nav-link">Bookings</Link>
-          <Link to="/reviews" className="nav-link">Awards & Reviews</Link>
-          <Link to="/gallery" className = "nav-link">Gallery</Link>
+          <Link
+    activeClass="active"
+    className = "nav-link"
+    to="home"
+    spy={true}
+    smooth={true}
+    offset={-70}
+    duration={600}
+>Home</Link>
+<Link
+    activeClass="active"
+    className = "nav-link"
+    to="about"
+    spy={true}
+    smooth={true}
+    offset={-70}
+    duration={600}
+>Bio</Link>
+<Link
+    activeClass="active"
+    className = "nav-link"
+    to="bookingForm"
+    spy={true}
+    smooth={true}
+    offset={-70}
+    duration={600}
+>Bookings</Link>
+<Link
+    activeClass="active"
+    className = "nav-link"
+    to="reviews"
+    spy={true}
+    smooth={true}
+    offset={-70}
+    duration={600}
+>Awards & Reviews</Link>
+<Link
+    activeClass="active"
+    className = "nav-link"
+    to="mixPlayer"
+    spy={true}
+    smooth={true}
+    offset={-70}
+    duration={600}
+>DJ Mixes</Link>
+<Link
+    activeClass="active"
+    className = "nav-link"
+    to="galleryImage"
+    spy={true}
+    smooth={true}
+    offset={-70}
+    duration={600}
+>Gallery</Link>
           </div>
           </nav>
         </div>
    
+   <div id = "home">
      <Home/>
+   </div>
+   <div id = "about">
      <About/>
+   </div>
+   <div id = "bookingForm">
      <BookingForm/>
+   </div>
+   <div id = "reviews">
      <Reviews/>
+   </div>
+   <div id = "mixPlayer">
      <MixPlayer/>
+   </div>
+   <div id = "galleryImage">
      <GalleryImage/>
+   </div>
+
    <Routes>
        <Route path="/gallery" element={<Gallery/>}/>
    </Routes>
