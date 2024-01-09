a452ffdbfcbfbd135af22ee85508b5e16dfaddf8
Ashley McKellar <ashley.l.mckellar@gmail.com>
2024-01-08 22:40:49 -0800

    debugging, will add dynamic style class to iframe div

--- client/componenets/MixPlayer.jsx
+++ client/componenets/MixPlayer.jsx
@@ -1,27 +1,26 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import ReactPlayer from 'react-player'
-// import IndMusic from './IndMusic'
-import './style.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons'
+import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faChevronLeft,
+    faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "https://widget.mixcloud.com/media/js/widgetApi.js";
 
 const MixPlayer = () => {
-    const iframeRef = useRef(null)
-    // const { dataState } = useContext(UserContext);
     const [mixes, setMixes] = useState([]);
-    const [selectedMix, setSelectedMix] = useState(null);
     const [isClicked, setIsClicked] = useState(false);
-    const [isPlaying, setisPlaying] = useState(false)
+    const [selectedMixIndex, setSelectedMixIndex] = useState(null);
+    const [iframeLoaded, setIframeLoaded] = useState(false);
+    const [widgets, setWidgets] = useState({});
 
-
-        const handleFocus = () => {
-            console.log('focused')
-        }
- 
+    useEffect(() => {
+        console.log(window.Mixcloud);
+    }, []);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -30,15 +29,14 @@ const MixPlayer = () => {
                     "https://api.mixcloud.com/djbentleytaylor/cloudcasts"
                 );
                 const data = response.data.data;
-                console.log(data);
                 const mixTiles = data.map((mix) => ({
                     name: mix.name,
                     url: mix.url,
                     pictures: mix.pictures && mix.pictures["320wx320h"],
                 }));
 
-                setMixes(mixTiles)
-                console.log(mixes);
+                setMixes(mixTiles);
+                console.log(mixTiles);
             } catch (error) {
                 console.error("Error fetching Mixcloud data:", error);
             }
@@ -46,88 +44,124 @@ const MixPlayer = () => {
         fetchData();
     }, []);
 
+    useEffect(() => {
+        setIframeLoaded(true);
+        console.log('iframe loaded!')
+    }, []);
+
     const handleMixClick = (index) => {
-        setSelectedMix(index);
+        setSelectedMixIndex(index);
         setIsClicked(true);
-        setisPlaying(true)
-        console.log()
+        handleWidgetPlayEvent();
+        const iframeId = `my-widget-iframe-${index}`;
+        const widget = window.Mixcloud.PlayerWidget(
+            document.getElementById(iframeId)
+        );
+        const pauseListener = () => {
+            console.log("Widget paused!");
+
+            // setWidgets((prevWidgets) => ({ ...prevWidgets, [index]: widget }));
+
+            // widget.ready.then(() => {
+            //     widget.events.play.on(() => console.log("It played!"));
+            // });
+        };
     };
 
-    const handleBlur = (index) => {
-        setIsClicked(false);
-        setSelectedMix(null);
-        setisPlaying(false)
-        console.log("blurred")
+    // const handleBlur = () => {
+    //     setIsClicked(false);
+    // };
+
+    const handleWidgetLoaded = () => {
+        setIframeLoaded(true);
     };
 
-// slider settings
-const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            initialSlide: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+    //will need to call the widget.events.play function on the widget that's clicked
+    //the widget that's clicked will need to be saved as a variable
+
+    const handleWidgetPlayEvent = (index) => {
+        // let widget;
+        // for (let id in widgets) {
+        //     widget = widgets[id];
+        //     widget.getIsPaused().then((isPaused) => {
+        //         if (isPaused) {
+        //             console.log(`Pausing widget ${id}`);
+        //             // widget.pause()
+        //         } else {
+        //             console.log(`played widget ${id}`);
+        //         }
+        //     });
+        // }
+        if (iframeLoaded) {
+            const iframeId = `my-widget-iframe-${index}`;
+            const widget = window.Mixcloud.PlayerWidget(
+                document.getElementById(iframeId)
+            );
+
+            widget.ready.then(() => {
+                widget.events.play.on(() => console.log("It played!"));
+            });
         }
-      ]
-  }
+    };
+
+    // slider settings
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    initialSlide: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
 
     return (
-        
         <div className="musicPlayer">
             <h1>Bentley's Mixes</h1>
-            {/* <FontAwesomeIcon icon={faChevronLeft} className = "chevronIcon"/> */}
-            <div className = "carousel">
+            <div className="carousel">
                 <Slider {...settings}>
-
-                {mixes.map((mix, index) => (
-                    <div
-                        className="mix-box"
-                        key={index}
-                        onClick={() => handleMixClick(index)}
-                        onBlur={handleBlur}
-                    >
-                        {selectedMix !== null && isClicked ? (
-                            <div
-                                onBlur={handleBlur}>
-                                <iframe
-                                    width="30%"
-                                    height="400"
-                                    title={mixes[selectedMix].name}
-                                    src={`https://player-widget.mixcloud.com/widget/iframe/?light=1&feed=${encodeURIComponent(
-                                        mix.url
-                                    )}`}
-                                    frameBorder="0"
+                    {mixes.map((mix, index) => (
+                        <div
+                            className="mix-box"
+                            key={index}
+                            onClick={() => handleMixClick(index)}
+                            // onBlur={handleBlur}
+                        >
+                            <div>
                                 
-                                ></iframe>
+                                    <iframe
+                                        id={`my-widget-iframe-${index}`}
+                                        onLoad={() => handleWidgetLoaded(index)}
+                                        width="30%"
+                                        height="400"
+                                        title={mix.name}
+                                        src={`https://player-widget.mixcloud.com/widget/iframe/?light=1&feed=${encodeURIComponent(
+                                            mix.url
+                                        )}`}
+                                        frameBorder="0"
+                                        className="dynamicClass"
+                                    ></iframe>
+                                
                             </div>
-                        ) : (
-                            <div
-                                onBlur={handleBlur}>
- {/* not displaying the names for a cleaner mor minimal look */}
-                                {/* <h3>{mix.name}</h3> */}
-                                <img src={mix.pictures} alt={mix.name} />
-                            </div>
-                        )}
-                    </div>
-                ))}
+                        </div>
+                    ))}
                 </Slider>
             </div>
-            {/* <FontAwesomeIcon icon={faChevronRight} className = "chevronIcon" /> */}
         </div>
     );
 };
