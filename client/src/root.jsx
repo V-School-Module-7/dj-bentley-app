dcc1b2bcab8357cb8cc506104bff2e983a6e967e
Ashley McKellar <ashley.l.mckellar@gmail.com>
2024-01-17 22:41:16 -0800

    created custom modal for form confirmation

--- client/src/root.jsx
+++ client/src/root.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '../componenets/App.jsx'
-
+import Modal from 'react-modal';
 
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+Modal.setAppElement('#root');
 root.render(
   <BrowserRouter>
 		<App />
