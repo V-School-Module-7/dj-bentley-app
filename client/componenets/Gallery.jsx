d88d63c170eb55a995b95964bf951419e32fb9de
valerie smith <valerie.plante.11@gmail.com>
2024-01-31 19:50:51 -0500

    saved all the images

--- client/componenets/Gallery.jsx
+++ client/componenets/Gallery.jsx
@@ -32,7 +32,7 @@ export default function Gallery() {
     const photosArr = [
         photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8, photo9, photo10, photo11, photo12, photo13, photo14, photo15, photo16
     ]
-
+    //
     const [showCarousel, setShowCarousel] = useState(false)
     const [selectedPhotoIndex, setSelectedPhotoIndex] = useState(null)
 
