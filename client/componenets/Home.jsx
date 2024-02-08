93ee399097a59eb083a89fcac29742245315a0e8
valerie smith <valerie.plante.11@gmail.com>
2024-02-08 11:33:07 -0500

    copied v2 site

---
+++ client/componenets/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css'
+import homeImage from "../assets/homeImage.jpg"
+
+export default function Home() {
+
+    return (
+        <div className="homeContainer">
+            <img className="homeImage" src={homeImage} ></img>
+        </div>
+    );
+}
---
+++ client/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'static',
+    assetsInclude: ["**/*.JPG"],
+    sourcemap: false,
+  },
+  base: '/'
+});
+// adjust defineConfig for deployment on render
